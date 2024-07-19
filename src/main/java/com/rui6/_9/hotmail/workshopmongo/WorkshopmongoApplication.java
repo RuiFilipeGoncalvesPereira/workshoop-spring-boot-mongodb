fab26c5913bbb4f669ca017923e35a8fe4cae806
Rui Pereira <rui6.319@hotmail.com>
2024-07-19 11:54:09 +0100

    Primeiro commit - projecto criado

---
+++ src/main/java/com/rui6/_9/hotmail/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.rui6._9.hotmail.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rui6/_9/hotmail/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rui6._9.hotmail.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
