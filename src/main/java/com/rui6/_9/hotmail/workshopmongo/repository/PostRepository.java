3eacd7202882f4c323fc3257339c190e1a4ea946
Rui Pereira <rui6.319@hotmail.com>
2024-07-24 15:01:45 +0100

    Simple Query with @query

--- src/main/java/com/rui6/_9/hotmail/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rui6/_9/hotmail/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.rui6._9.hotmail.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rui6._9.hotmail.workshopmongo.domain.Post;
@@ -12,7 +13,8 @@ import com.rui6._9.hotmail.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' }}") 
+	List<Post> searchTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
     
 }
